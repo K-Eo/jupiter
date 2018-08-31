665ae72fbc169a9ae8a524eade6a318ac2631ae2
Diego Lopez <aud0l@hotmail.com>
2018-08-30 19:34:59 -0500

    Instalar react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import App from './App'
@@ -18,7 +19,9 @@ firebase.auth().onAuthStateChanged(state => {
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
