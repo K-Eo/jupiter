58b184926fab2b877666dcc8756d13fb8cf8374b
Diego Lopez <aud0l@hotmail.com>
2018-08-31 02:02:19 -0500

    Corregir: redux elimina datos del usuario autenticado

--- src/auth/reducers.js
+++ src/auth/reducers.js
@@ -14,7 +14,8 @@ export const auth = (state = authInitialState, action) => {
     case types.LOGIN:
       return Object.assign({}, state, { signedIn: true, userId: action.userId })
     case types.LOGOUT:
-    default:
       return { mode: constants.PASSENGER, signedIn: false, userId: null }
+    default:
+      return state
   }
 }
