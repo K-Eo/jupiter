14138c6e69a0eaf3e9b671b50f2f9c8314e3d1fc
Diego Lopez <aud0l@hotmail.com>
2018-09-01 02:43:25 -0500

    Mostrar empty state cuando no hay viajes

--- src/trips/components/TripsList.js
+++ src/trips/components/TripsList.js
@@ -2,18 +2,31 @@ import React from 'react'
 
 import Trip from './TripContainer'
 
-const TripList = ({ trips }) => {
-  if (trips.length === 0) {
+const TripList = ({ trips, listen }) => {
+  if (!listen) {
     return null
   }
 
+  if (trips.length === 0) {
+    return (
+      <div className="col-12">
+        <div className="jumbotron text-center m-0">
+          <i className="fas fa-circle-notch fa-2x fa-spin text-gray mb-3" />
+          <p className="m-0 text-secondary">Esperando pasajeros</p>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className="card mt-4">
-      <ul className="list-group list-group-flush">
-        {trips.map(trip => (
-          <Trip key={trip.id} {...trip} />
-        ))}
-      </ul>
+    <div className="col-12">
+      <div className="card">
+        <ul className="list-group list-group-flush">
+          {trips.map(trip => (
+            <Trip key={trip.id} {...trip} />
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
--- src/trips/components/TripsListContainer.js
+++ src/trips/components/TripsListContainer.js
@@ -4,6 +4,7 @@ import TripsList from './TripsList'
 
 const mapStateToProps = state => ({
   trips: Object.values(state.entities.trips),
+  listen: state.driver.listen,
 })
 
 export default connect(mapStateToProps)(TripsList)
