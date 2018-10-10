cb9ef5f48ff9e3e7c2325efdf026651ff6745144
anjelhm <anjelhm@gmail.com>
2018-10-10 16:35:43 -0500

    Dar formato a archivos

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import Home from './components/Home';
+import Home from './components/Home'
 
 const Routes = () => (
   <Switch>
-    <Route exact path="/" component={Home}/>
+    <Route exact path="/" component={Home} />
   </Switch>
 )
 
